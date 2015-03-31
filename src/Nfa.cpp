b0ed95e94312f4a38666dd9bf379b2d493b0c118
Manyanda Chitimbo <chitson1990@gmail.com>
2015-03-31 16:43:41 +0200

    Latest

--- src/Nfa.cpp
+++ src/Nfa.cpp
@@ -91,6 +91,83 @@ void Nfa::unify(const Nfa *toUnify) {
 	final = newFinal;
 }
 
+void Nfa::eliminate_eps() {
+	list<State*> _states;
+	list<State*> _states1;
+	list<State*> _stateEPS = getStatesWithEpSTransition();
+	bool isEmpty = _stateEPS.empty();
+	State* current;
+	list<int> symbols;
+
+	while (!isEmpty) {
+		cout << _stateEPS.size() << " Size\n";
+		current = _stateEPS.front();
+		_stateEPS.pop_front();
+		_states = current->getTransitions(State::EPS);
+		for (auto& s : _states) {
+			symbols = s->get_symbols();
+			if (s == final) {
+				if (finals.find(current) == finals.end()) {
+					finals.insert(current);
+				}
+			}
+			for (auto symbol : symbols) {
+				_states1 = s->getTransitions(symbol);
+				// Create a transitions
+				for (auto e1 : _states1) {
+					current->setTransition(symbol, e1);
+				}
+			}
+			current->delete_transitions(State::EPS);
+			if(!isAccessible(s)) {
+				states.erase(s);
+			}
+		}
+		states.insert(current);
+		_stateEPS = getStatesWithEpSTransition();
+		isEmpty = _stateEPS.empty();
+	}
+}
+
+bool exist(list<State*> states, State* state) {
+	for (auto& stat : states) {
+		if (stat == state)
+			return true;
+	}
+	return false;
+}
+
+bool Nfa::isAccessible(State* state) {
+	list<int> symbols;
+	list<State*> _states;
+	for (auto& _state : states) {
+		symbols = state->get_symbols();
+		for (auto s : symbols) {
+			_states = state->getTransitions(s);
+			if (exist(_states, state))
+				return true;
+		}
+	}
+	return false;
+}
+
+Nfa* Nfa::determinize() {
+
+}
+
+list<State*> Nfa::getStatesWithEpSTransition() {
+	list<State*> temp;
+	list<State*> result = list<State*>();
+	list<State*>::iterator it;
+	for (auto state : states) {
+		temp = state->getTransitions(State::EPS);
+		if (!temp.empty()) {
+			result.push_back(state);
+		}
+	}
+	return result;
+}
+
 void Nfa::apply_cardinality(enum card_t type) {
 	State *newInitial = new State();
 	State *newFinal = new State();
@@ -121,30 +198,30 @@ void Nfa::apply_cardinality(int min, int max) {
 	}
 
 	switch (max) {
-		case SIMPLE_COUNT:
-			break;	//do_nothing
-		case UNBOUNDED: {
+	case SIMPLE_COUNT:
+		break;	//do_nothing
+	case UNBOUNDED: {
+		Nfa *toAppend = new Nfa(orig);
+		toAppend->apply_cardinality(KLEENE_STAR);
+		concatenate(toAppend);
+		break;
+	}
+	default: {
+		list<State *> abortStates;
+		abortStates.push_back(final);
+
+		for (; i < max; i++) {
 			Nfa *toAppend = new Nfa(orig);
-			toAppend->apply_cardinality(KLEENE_STAR);
 			concatenate(toAppend);
-			break;
-		}
-		default: {
-			list<State *> abortStates;
 			abortStates.push_back(final);
-
-			for (; i < max; i++) {
-				Nfa *toAppend = new Nfa(orig);
-				concatenate(toAppend);
-				abortStates.push_back(final);
-			}
-
-			for (auto& abort : abortStates) {
-				(*states.find(abort))->setTransition(State::EPS, newFinal);
-			}
-			states.insert(newFinal);
-			final = newFinal;
 		}
+
+		for (auto& abort : abortStates) {
+			(*states.find(abort))->setTransition(State::EPS, newFinal);
+		}
+		states.insert(newFinal);
+		final = newFinal;
+	}
 	}
 }
 
@@ -169,7 +246,6 @@ int Nfa::rec_evaluate(string in, State *state) {
 		if (rec_evaluate(in.substr(1), next_state) == ACCEPT)
 			return ACCEPT;
 
-
 	for (auto& eps_state : eps_states)
 		if (rec_evaluate(in, eps_state) == ACCEPT)
 			return ACCEPT;
@@ -181,78 +257,95 @@ int Nfa::rec_evaluate(string in, State *state) {
 	return REJECT;
 }
 
+int Nfa::rec_evaluate_second(string word, State* state) {
+	if(finals.size()>0) {
+
+	} else
+		return rec_evaluate(word,state);
+}
+
 int Nfa::evaluate(string in) {
 	int result = rec_evaluate(in, initial);
 	cout << "\n" << in << ": " << ((result == ACCEPT) ? "YES" : "NO") << "\n\n";
 	return result;
 }
 
+
 Nfa::~Nfa() {
-	// TODO Auto-generated destructor stub
+// TODO Auto-generated destructor stub
 }
 
 void Nfa::print() {
-	cout << "Initial: " << initial->getId() << ", final: " << final->getId() << "\n";
+	cout << "Initial: " << initial->getId() << ", final: " << final->getId()
+			<< "\n";
 	for (auto& state : states) {
 		state->print();
 	}
 	cout << "\n\n";
 }
 
-
-int main () {
+int main() {
 	/*
-	Nfa *a = new Nfa('a');
-	a->print();
-	a->evaluate("a");
+	 Nfa *a = new Nfa('a');
+	 a->print();
+	 a->evaluate("a");
 
-	Nfa *b = new Nfa('b');
-	b->print();
-	b->evaluate("b");
-	b->evaluate("a");
+	 Nfa *b = new Nfa('b');
+	 b->print();
+	 b->evaluate("b");
+	 b->evaluate("a");
 
-	//a->unify(b);
-	a->concatenate(b);
-	a->print();
-	a->evaluate("a");
-	a->evaluate("b");
-	a->evaluate("ab");
+	 //a->unify(b);
+	 a->concatenate(b);
+	 a->print();
+	 a->evaluate("a");
+	 a->evaluate("b");
+	 a->evaluate("ab");
 
 
-	a->apply_cardinality(3, 5);
-	a->print();
-	a->evaluate("ab");
-	a->evaluate("abab");
-	a->evaluate("ababab");
-	a->evaluate("abababab");
-	a->evaluate("ababababab");
-	a->evaluate("abababababab");
-	*/
-
+	 a->apply_cardinality(3, 5);
+	 a->print();
+	 a->evaluate("ab");
+	 a->evaluate("abab");
+	 a->evaluate("ababab");
+	 a->evaluate("abababab");
+	 a->evaluate("ababababab");
+	 a->evaluate("abababababab");
+	 */
 
 	list<int> chars;
 	chars.push_back('a');
-	chars.push_back('b');
-	chars.push_back('c');
+//chars.push_back('b');
+//chars.push_back('c');
 	Nfa *a = new Nfa(chars, false);
 	a->endAnywhere();
 	a->startAnywhere();
 
-	Nfa *b = new Nfa('d');
-	Nfa *c = new Nfa('e');
-	b->unify(c);
+	Nfa *b = new Nfa('b');
+//Nfa *c = new Nfa('e');
+//b->unify(c);
 	b->apply_cardinality(PLUS);
 	b->endAnywhere();
 
 	a->unify(b);
+
+	a->evaluate("ab");
+
+	a->evaluate("a");
+	a->evaluate("b");
+
+	a->print();
+	a->eliminate_eps();
+
+	a->evaluate("ab");
+
 	a->print();
 	a->evaluate("a");
 	a->evaluate("b");
-	a->evaluate("c");
-	a->evaluate("abc");
-	a->evaluate("d");
-	a->evaluate("dedede");
-	a->evaluate("fdedede");
-
+//a->evaluate("c");
+//a->evaluate("abc");
+//a->evaluate("d");
+//a->evaluate("dedede");
+//a->evaluate("fdedede");
 }
 
--- src/Nfa.h
+++ src/Nfa.h
@@ -24,10 +24,10 @@ enum card_t {KLEENE_STAR, OPTION, PLUS};
 
 class Nfa {
 	private:
-	Nfa(const Nfa *);
 	unordered_set<State *> states;
 	State *final;
 	State *initial;
+	unordered_set<State*> finals;
 
 	int rec_evaluate(string, State *);
 
@@ -41,9 +41,14 @@ class Nfa {
 	void unify(const Nfa *);
 	void apply_cardinality(enum card_t);
 	void apply_cardinality(int, int);
-
+	void eliminate_eps();
+	list<State*> getStatesWithEpSTransition();
+	Nfa* determinize();
 	void print();
 	int evaluate(string);
+	int rec_evaluate_second(string);
+	Nfa(const Nfa *);
+	bool isAccessible(State*);
 };
 
 #endif /* SRC_NFA_H_ */
